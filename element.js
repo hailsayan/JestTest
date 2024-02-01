9fbb4cec8aa4500de4444ef5eccad360df3540ef
Psyon <143930298+hailsayan@users.noreply.github.com>
2024-02-01 21:29:06 +0330

    test numbers

---
+++ element.js
@@ -0,0 +1,8 @@
+// greater than equal
+function gte(a, b){
+    if(a > b) return true;
+    if(a < b) return false;
+    if(a == b) return true;
+}
+
+module.exports = gte;
--- tests/element.test.js
+++ tests/element.test.js
@@ -1,3 +1,13 @@
-test("Arto", ()=>{
-    throw new Error('something went wrong')
+const gte = require('./../element');
+test("return true if a is greater than b", ()=>{
+    const result = gte (10, 5);
+    expect(result).toBe(true);
+});
+test("return false if a is less than b", ()=>{
+    const result = gte (5, 10);
+    expect(result).toBe(false);
+});
+test("return true if a is equal than b", ()=>{
+    const result = gte (5, 5);
+    expect(result).toBe(true);
 });
